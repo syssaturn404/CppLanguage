4d8f7b846bd6173382037688a7b880e2787fbdbb
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-14 13:39:31 +0700

    Create Basic_Syntax.cpp

---
+++ Basic_Syntax.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // include
+using namespace std; // bagian deklarasi
+
+int main(){
+	
+  // output
+	cout << "=======SELAMAT DATANG DI PROGRAM PERTAMA SAYA=======" << endl;
+	cout << "Program ini adalah program untuk \n";
+	cout << "Menampilkan output ke layer komputer \n";
+	cout << "Informasi ini sangat penting \n";
+	cout << "====================================================" << endl;
+	cout << endl;
+	cout << "Program by Ridwan" << endl;
+	
+	return 0;
+}
